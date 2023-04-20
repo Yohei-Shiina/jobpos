f5e5b5990dc0e70c036c9f93896e7f49f76a8e1f
Yohei Shiina <ysys.j.0125@gmail.com>
2023-04-20 15:08:00 +0000

    query a database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,27 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+  const createResult = await prisma.post.create({
+    data: {
+      title: "ルームメイト10名募集!",
+      content: "部屋に空きができたので、ルームメイト(男性女性どちらでも)を10人募集します。",
+    },
+  });
+  console.log({ createResult });
+
+  const updateResult = await prisma.post.update({
+    where: { id: createResult.id },
+    data: {
+      title: "ルームメイト1名募集!",
+      content: "部屋に空きができたので、ルームメイト(男性女性どちらでも)を1人募集します。",
+    },
+  });
+  console.log({ updateResult });
+
   const allPosts = await prisma.post.findMany();
-  console.log(allPosts);
+  console.log({ allPosts });
+
+  await prisma.post.deleteMany({});
 }
 
 main()
